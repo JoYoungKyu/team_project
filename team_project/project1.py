35d6c882d7e74c3ee13ec0755fba04e071ea6e09
JoYoungKyu <137609627+JoYoungKyu@users.noreply.github.com>
2025-04-10 20:45:39 +0900

    Update project1.py

--- team_project/project1.py
+++ team_project/project1.py
@@ -1,6 +1,5 @@
 from roboflow import Roboflow
 rf = Roboflow(api_key="tEFOW2xP2wtvNyZmpXnL")
 project = rf.workspace("jyk-ucnhk").project("jyk")
-version = project.version(7)
+version = project.version(11)
 dataset = version.download("yolov11")
-                
